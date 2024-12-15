2a25a2cddfe7588a9c110ad39e0324c84118e5e3
rizzle-rohit <rohit.y@thesilverlabs.com>
2024-12-16 01:00:15 +0530

    export app

--- src/server.js
+++ src/server.js
@@ -19,7 +19,8 @@ app.get('/health', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+export default app;
 
