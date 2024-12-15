7bec6610e218d0c84e26fb235b625042a7dbb432
rizzle-rohit <rohit.y@thesilverlabs.com>
2024-12-16 04:29:03 +0530

    allowed all origin

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,12 +1,12 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow only this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  exposedHeaders: ['Content-Range', 'X-Content-Range'], // Expose these headers
-  credentials: true, // Allow credentials (cookies, authorization headers, etc)
-  maxAge: 86400, // Cache preflight requests for 24 hours
+  origin: '*', // Allow only this origin
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+//   exposedHeaders: ['Content-Range', 'X-Content-Range'], // Expose these headers
+//   credentials: true, // Allow credentials (cookies, authorization headers, etc)
+//   maxAge: 86400, // Cache preflight requests for 24 hours
 };
 
 // Export the CORS middleware with options
